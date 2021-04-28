103fb7c56cde6d24135e0644ffa4d96b19f93c0f
apollo141 <aboodyx141@gmail.com>
2021-04-28 22:32:09 +0300

    add another one

---
+++ html/Untitled-1.cpp
@@ -0,0 +1,19 @@
+
+# include<iostream.h>
+# include<conio.h>
+class rectangle
+{
+private:
+int l;
+int w;
+public:
+rectangle(int a ,int b)
+{
+l=a;
+w=b;
+}
+void area( )
+{
+cout<<"Area of rectangle is : "<<l*w <<endl;
+}
+}; 
